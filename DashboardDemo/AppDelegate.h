3b3cf2386273c5457243510615ce2b3733ee6dc7
王傲云 <wangaoyun@wangaoyundeiMac.local>
2018-09-03 18:01:23 +0800

    Initial Commit

---
+++ DashboardDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DashboardDemo
+//
+//  Created by 王傲云 on 2018/9/3.
+//  Copyright © 2018 王傲云. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DashboardDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DashboardDemo
+//
+//  Created by 王傲云 on 2018/9/3.
+//  Copyright © 2018 王傲云. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
